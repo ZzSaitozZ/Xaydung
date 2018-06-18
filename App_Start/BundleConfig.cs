48df44dcbdd5e81e6bf75d37b51d6e888a7e0e2c
ZzSaitozZ <hiragasaito951@gmail.com>
2018-06-18 10:25:04 +0700

    Initial commit

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Xaydung
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Resrouce/JS/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Resrouce/JS/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Resrouce/JS/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Resrouce/JS/Scripts/bootstrap.js",
+                      "~/Resrouce/JS/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Resrouce/CSS/Content/bootstrap.css",
+                      "~/Resrouce/CSS/Content/site.css"));
+        }
+    }
+}
